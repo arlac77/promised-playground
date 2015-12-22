6a135926da6a2a6ea9437403f72537445f8fa69b
Markus Felten <Markus.Felten@gmx.de>
2015-12-23 00:46:06 +0100

    wip

--- chaining.js
+++ chaining.js
@@ -1,7 +1,7 @@
 "use strict";
 
 
-let all = [1, 2, 3, 4, 5, 6];
+var all = [1, 2, 3, 4, 5, 6];
 
 
 initialize('yellow');
@@ -11,28 +11,49 @@ function initialize(color) {
   elem.innerHTML = all.map(i => `<circle id="c${i}" cx="80" cy="${i*25}" r="10" style="fill: ${color}"/>`).join('');
 }
 
+
+function delayPromise(resolve, timeout) {
+  return new Promise(function (fulfill, reject) {
+    setTimeout(() => {
+      fulfill(resolve);
+    }, 3000);
+  });
+}
+
 function start() {
   initialize('black');
 
   var ps = all.map(
-    n => new Promise(function (fulfill, reject) {
+    n => {
       const elem = document.getElementById('c' + n);
-      elem.setAttribute("style", "fill: red");
-      setTimeout(() => {
+      return delayPromise(n, 3000).then(function (f) {
         elem.setAttribute("style", "fill: blue");
-        fulfill(n);
-      }, 3000);
-    })
+        return f;
+      })
+    }
   );
 
-  for (let i = 0; i < ps.length - 1; i++) {
-    ps[i].then(r => {
-      r(ps[i + 1]);
-    });
-  }
+
+  /*
+  n => new Promise(function (fulfill, reject) {
+    const elem = document.getElementById('c' + n);
+    elem.setAttribute("style", "fill: red");
+    setTimeout(() => {
+      elem.setAttribute("style", "fill: blue");
+      fulfill(n);
+    }, 3000);
+  })*/
+
+  /*
+    for (let i = 0; i < ps.length - 1; i++) {
+      ps[i].then(r => {
+        r(ps[i + 1]);
+      });
+    }
+  */
 
   ps[ps.length - 1].then(v => {
-    initialize('black');
+    initialize('green');
   });
 }
 
