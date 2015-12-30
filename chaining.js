5d3d4549143daf6928d5cf0e1a1d02a7d8195522
Markus Felten <Markus.Felten@gmx.de>
2015-12-30 22:43:12 +0100

    fix: chaining with delay now works

--- chaining.js
+++ chaining.js
@@ -1,73 +1,42 @@
 "use strict";
 
 
-var all = [1, 2, 3];
-
+var all = [1, 2, 3, 4, 5, 6];
 
 initialize('yellow');
 
 function initialize(color) {
   const elem = document.getElementById('sample1');
-  elem.innerHTML = all.map(i =>
-    `<circle id="c${i}" cx="80" cy="${i*25}" r="10" style="fill: ${color}"><animateMotion dur="12s" values="80,${i*25};400,${i*25}"/></circle>`
+  elem.innerHTML = all.map(id =>
+    `<circle id="c${id}" cx="80" cy="${id*25}" r="10" style="fill: ${color}"></circle>`
   ).join('');
 }
 
-function setState(elem, color, position) {
+function setState(id, color, position, run) {
+  const elem = document.getElementById('c' + id);
+
   elem.setAttribute("cx", position);
+  elem.setAttribute("cy", id * 25);
   elem.setAttribute("style", `fill: ${color}`);
+  if (run) {
+    elem.innerHTML = `<animateMotion dur="12s" values="${position},${id*25};400,${id*25}"/></circle>`;
+  } else {
+    elem.innerHTML = '';
+  }
 }
 
-
-function delayPromise(resolve, timeout) {
-  return new Promise(function (fulfill, reject) {
-    setTimeout(() => {
-      fulfill(resolve);
-    }, 3000);
-  });
+function delayedPromise(delay, value) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(value), delay);
+  })
 }
 
 function start() {
-  initialize('black');
-
-  var ps = all.map(
-    n => {
-      const elem = document.getElementById('c' + n);
-      return delayPromise(n, 3000).then(function (f) {
-        setState(elem, 'blue', 80);
-        return f;
-      })
-    }
+  all.reduce((previous, id) => {
+    return previous.then(f => {
+      setState(id, 'blue', 80, true);
+    }).then(() => delayedPromise(2000));
+  }, Promise.resolve()).then(
+    () => initialize('yellow')
   );
-
-
-  /*
-  n => new Promise(function (fulfill, reject) {
-    const elem = document.getElementById('c' + n);
-    elem.setAttribute("style", "fill: red");
-    setTimeout(() => {
-      elem.setAttribute("style", "fill: blue");
-      fulfill(n);
-    }, 3000);
-  })*/
-
-  /*
-    for (let i = 0; i < ps.length - 1; i++) {
-      ps[i].then(r => {
-        r(ps[i + 1]);
-      });
-    }
-  */
-
-  ps[ps.length - 1].then(v => {
-    initialize('green');
-  });
 }
-
-/*
-.reduce((p, c) => {
-  return p ? p.then(c) : c;
-}).then(
-  (v) => console.log(`done`)
-);
-*/
